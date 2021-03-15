caa0c6f27969ca4ffcb76f6f41067efd73c8a958
mattyhempstead <matty.hempstead@gmail.com>
2021-03-16 02:29:08 +1100

    Fix view spammer

--- src/scripts/super-view.js
+++ src/scripts/super-view.js
@@ -12,7 +12,7 @@ console.log('edhack activated')
 // (although maybe page updates should be after /api/threads/* as this is what returns the question body)
 
 let xToken        // The current x-token extracted from request headers
-let thread        // The current viewing thread
+let viewURL
 let checkbox      // The BUMP toggle switch checkbox
 let viewInterval  // The interval used to spam views
 
@@ -27,7 +27,7 @@ fetch = (req, options={}) => {
   // This just a safety feature because edstem is single page which ensures that when users navigate 
   // away from the discussion, the spammer is stopped
   // This will probably also cause the spammer to disable seemingly randomly, however that is probably fine
-  if (req.url.indexOf(thread) === -1) toggleSuperView(false)
+  if (req.url !== viewURL) toggleSuperView(false)
 
 
   // Keep the global x-token variable updated
@@ -38,44 +38,44 @@ fetch = (req, options={}) => {
 
 
   // If user sends a view request, create/configure BUMP switch
-  if (req.url.endsWith('/view')) {
+  if (req.url.endsWith('?view=1')) {
 
-    let urlSplit = req.url.split('/')
+    getElement('dtf-head-layout').then((viewDiv) => {
+      viewDiv = viewDiv.children[1];
 
-    let viewDiv = document.getElementsByClassName('dtf-head-layout')[0]
-    // let superViewButton = document.createElement('button')
+      // Add bump switch if it does not already exist
+      if (viewDiv.getElementsByClassName('super-view').length === 0) {
+
+        // For some reason you need to add the switch this way rather than appending innerHTML to viewDiv
+        // If you don't, the page no longer updates the view counter live
+        superViewDiv = document.createElement('div')
+        superViewDiv.className = 'dtf-head-action super-view'
+        superViewDiv.innerHTML = `
+          <label class="switch">
+            <input type="checkbox" class="super-view-checkbox">
+            <span class="slider"></span>
+          </label>
+          <p class="dtf-head-action-label">BUMP</p>
+        `
+        viewDiv.appendChild(superViewDiv)
+
+        // Listen for changes in the bump switch
+        checkbox = viewDiv.getElementsByClassName('super-view-checkbox')[0]
+        checkbox.addEventListener('change', (evt) => {
+          toggleSuperView(evt.target.checked)
+        })
+
+      }
 
 
-    // Add bump switch if it does not already exist
-    if (viewDiv.getElementsByClassName('super-view').length === 0) {
+    })
 
-      // For some reason you need to add the switch this way rather than appending innerHTML to viewDiv
-      // If you don't, the page no longer updates the view counter live
-      superViewDiv = document.createElement('div')
-      superViewDiv.className = 'dtf-head-action super-view'
-      superViewDiv.innerHTML = `
-        <label class="switch">
-          <input type="checkbox" class="super-view-checkbox">
-          <span class="slider"></span>
-        </label>
-        <p class="dtf-head-action-label">BUMP</p>
-      `
-      viewDiv.appendChild(superViewDiv)
-
-
-      // Listen for changes in the bump switch
-      checkbox = viewDiv.getElementsByClassName('super-view-checkbox')[0]
-      checkbox.addEventListener('change', (evt) => {
-        toggleSuperView(evt.target.checked)
-      })
-
-    }
 
     // If thread changes, turn off checkbox
-    let newThread = urlSplit.splice(-2, 1)[0]
-    if (newThread !== thread) {
-      // console.log(`Changing threads from ${thread} to ${newThread}`)
-      thread = newThread
+    let newViewURL = req.url;
+    //console.log(newViewURL)
+    if (newViewURL !== viewURL) {
+      viewURL = newViewURL
       toggleSuperView(false)
     }
 
@@ -98,8 +98,8 @@ function toggleSuperView(state) {
       // Start a view spammer interval
       viewInterval = setInterval(() => {
         fetch(
-          `https://edstem.org/api/threads/${thread}/view?`, {
-          method: 'POST',
+          viewURL, {
+          method: 'GET',
           headers: {
             'x-token': xToken
           }
@@ -110,3 +110,45 @@ function toggleSuperView(state) {
   }
 }
 
+
+/*
+ * A function that returns a Promise which resolves to a desired element
+ * when it appears in the DOM.
+ */
+const getElement = (className) => {
+  return new Promise((resolve, reject) => {
+   
+    // Instantly return element if it already exists in DOM
+    let el = document.getElementsByClassName(className);
+    if (el.length === 1) {
+      resolve(el[0]);
+    } else if (el.length > 1) {
+      reject('Found two elements with class');
+    }
+
+    
+    // Create an observer that listens for object to be added to DOM
+    const observer = new MutationObserver((mutationsList, observer) => {
+      for (const mutation of mutationsList) {
+        if (mutation.type !== 'childList') continue;
+        if (mutation.addedNodes.length === 0) continue;
+
+        for (let i=0; i<mutation.addedNodes.length; i++) {
+          const addedNode = mutation.addedNodes[i];
+          if (addedNode.getElementsByClassName === undefined) continue;
+
+          const el = addedNode.getElementsByClassName(className);
+          if (el.length === 1) {
+            observer.disconnect();
+            resolve(el[0]);
+          } else if (el.length > 1) {
+            reject('Found two elements with class');
+          }
+        }
+
+      }
+    });
+    observer.observe(document.body, {childList:true, subtree:true});
+
+  })
+}
